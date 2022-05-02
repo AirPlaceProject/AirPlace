a14afc23effa0e3a3bcfeb374f4543f977690a1a
ZipiWerner <zipi100100@gmail.com>
2022-05-02 16:15:36 +0300

    Route2

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import './App.css';
 import PassengerCard from './PassengerCard'
 import SignIn from './login'
 import CircularIntegration from './placement'
-import FormConstrain from './formConstrain';
-import AutoGridNoWrap from './fly'
-import {  Route, Routes } from "react-router-dom";
-import Form2 from './form2';
 function App() {
   return (
   <>
-  <div className="App">
-     <Routes>
-      <Route path="formConstrain" element={<FormConstrain/>} /> 
-      <Route path="" element={<AutoGridNoWrap/>} />
-    </Routes>
-
-    </div>
+ <PassengerCard/>
+<CircularIntegration/>
+<SignIn/>
   </>
   );
 }
