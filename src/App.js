f2a3677d0c99a5aa9f0beb431d65724379b05ac6
naamafrank1 <naama0556772275@gmail.com>
2022-05-02 23:33:33 +0300

    fixLogin

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import PassengerCard from './PassengerCard'
-import LogIn from './login'
+import SignIn from './login'
 import CircularIntegration from './placement'
-import AutoGridNoWrap from './formConstrain'
-
+import FormConstrain from './formConstrain';
+import AutoGridNoWrap from './fly'
+import { Route, Routes } from "react-router-dom";
+import Form2 from './form2';
 function App() {
   return (
     <>
+      <div className="App">
+      <Routes>
+      <Route path="formConstrain" element={<FormConstrain/>} /> 
+      <Route path="" element={<AutoGridNoWrap/>} />
+    </Routes>
 
-      < LogIn/>
-      <PassengerCard />
-      <CircularIntegration />
-      <AutoGridNoWrap />
+      </div>
     </>
   );
 }
